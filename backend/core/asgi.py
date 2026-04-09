f404ddc0e73f24d98a2cbb3d71948760fba89a8e
Makc Krulevskiy <krulevckiym@gmail.com>
2026-04-09 20:35:41 +0300

    fix: initialize django before importing routes in asgi

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,10 +1,12 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from realtime.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
