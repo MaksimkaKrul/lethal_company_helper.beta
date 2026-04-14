a0f20c18a725bc6860c112e31ddc65aec5f7d6a4
Makc Krulevskiy <krulevckiym@gmail.com>
2026-04-15 00:08:12 +0300

    refactor(core): remove redundant view for serving frontend

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-from .views import home
+from django.urls import path, include, re_path
+from .views import index_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home),
     path("api/rooms/", include("rooms.urls")),
     path("api/users/", include("users.urls")),
     path("api/forum/", include("forum.urls")),
+    re_path(r"^.*$", index_view),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "home.html")
-
-def frontend(request):
+def index_view(request):
     return render(request, "index.html")
