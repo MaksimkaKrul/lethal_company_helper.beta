23182bea7e1c1a7fe7fb06cc2b2f0ade1daef473
Makc Krulevskiy <krulevckiym@gmail.com>
2026-04-14 20:48:36 +0300

    fix: made cleanup timer longer for better expirience

--- backend/rooms/management/commands/cleanup_rooms.py
+++ backend/rooms/management/commands/cleanup_rooms.py
@@ -7,10 +7,10 @@ from rooms.models import Room
 logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
-    help = "Видаляє покинуті кімнати, де немає гравців понад 2 хвилини"
+    help = "Видаляє покинуті кімнати, де немає гравців понад 10 хвилини"
 
     def handle(self, *args, **options):
-        threshold = timezone.now() - timedelta(minutes=2)
+        threshold = timezone.now() - timedelta(minutes=10)
         deleted_count, _ = Room.objects.filter(empty_since__lt=threshold).delete()
         
         if deleted_count > 0:
